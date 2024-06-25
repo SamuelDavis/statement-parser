3b308deb76c1d6a56ab5698b98cd1d721636ca1d
Samuel Davis <samueljakdavis@gmail.com>
2024-06-24 21:37:35 -0400

    wrap upload flow in dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
 import UploadFlow from "./UploadFlow";
+import { createSignal, Show } from "solid-js";
+import { NormalRow } from "./types.ts";
 
 function App() {
+  const [getOpen, setOpen] = createSignal(false);
+
+  function onSubmit(_: Event, name: string, rows: NormalRow[]) {
+    setOpen(false);
+    console.group(name);
+    console.log(rows);
+    console.groupEnd();
+  }
+
   return (
     <main>
-      <UploadFlow onSubmit={console.log} />
+      <header>
+        <button onClick={() => setOpen(true)}>Upload</button>
+      </header>
+      <Show when={getOpen()}>
+        <dialog open>
+          <article>
+            <header>
+              <a
+                role="button"
+                href="#"
+                rel="prev"
+                onClick={() => setOpen(false)}
+                aria-label="close"
+              />
+              <p>
+                <strong>Upload a new Bank statement</strong>
+              </p>
+            </header>
+            <UploadFlow onSubmit={onSubmit} />
+          </article>
+        </dialog>
+      </Show>
     </main>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 /* @refresh reload */
-import {render} from "solid-js/web";
+import { render } from "solid-js/web";
 import "@picocss/pico/css/pico.classless.min.css";
+import "./index.module.css";
 import App from "./App";
 
 render(() => <App />, document.body);
