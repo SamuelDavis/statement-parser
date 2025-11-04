6bd1d00c393203f5f3eede83d8dff04971acb77a
Samuel Davis <samueljakdavis@gmail.com>
2025-11-04 18:06:39 -0500

    oxford comma

--- src/Components/TransactionsSummary.tsx
+++ src/Components/TransactionsSummary.tsx
@@ -6,23 +6,19 @@ export default function TransactionsSummary(props: {
   transactions: Transaction[];
 }) {
   const getTotal = () =>
-    props.transactions.reduce(
-      (acc, transaction) => acc + transaction.amount,
-      0,
-    );
+    props.transactions.reduce((acc, tx) => acc + tx.amount, 0);
   const getInterval = () => {
     const end = props.transactions[0]?.date;
     const start = props.transactions[props.transactions.length - 1]?.date;
     if (start && end && !isEqual(start, end)) {
       const parts = formatDuration(
-        intervalToDuration({
-          start: new Date(start),
-          end: new Date(end),
-        }),
+        intervalToDuration({ start: new Date(start), end: new Date(end) }),
         { delimiter: ";" },
       ).split(";");
       const last = parts.pop();
-      return `${parts.join(", ")} and ${last}`;
+      if (parts.length === 0) return last;
+      if (parts.length === 1) return `${parts} and ${last}`;
+      return `${parts.join(", ")}, and ${last}`;
     }
     return "some time";
   };
