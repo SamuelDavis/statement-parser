cbd7b2ca07e03e90ae3be1d11ce7756c8741e71e
Samuel Davis <samueljakdavis@gmail.com>
2025-11-03 23:23:03 -0500

    add/test date-fns

--- src/Components/TransactionsSummary.tsx
+++ src/Components/TransactionsSummary.tsx
@@ -1,3 +1,4 @@
+import { formatDuration, intervalToDuration, isEqual } from "date-fns";
 import type { Transaction } from "../types";
 import HTMLNumber from "./HTMLNumber";
 
@@ -9,11 +10,28 @@ export default function TransactionsSummary(props: {
       (acc, transaction) => acc + transaction.amount,
       0,
     );
+  const getInterval = () => {
+    const end = props.transactions[0]?.date;
+    const start = props.transactions[props.transactions.length - 1]?.date;
+    if (start && end && !isEqual(start, end)) {
+      const parts = formatDuration(
+        intervalToDuration({
+          start: new Date(start),
+          end: new Date(end),
+        }),
+        { delimiter: ";" },
+      ).split(";");
+      const last = parts.pop();
+      return `${parts.join(", ")} and ${last}`;
+    }
+    return "some time";
+  };
   return (
     <span>
       <HTMLNumber value={props.transactions.length} />
       <span> transactions totalling </span>
-      <HTMLNumber value={getTotal()} highlight money />.
+      <HTMLNumber value={getTotal()} highlight money />
+      <span> over {getInterval()}</span>.
     </span>
   );
 }
